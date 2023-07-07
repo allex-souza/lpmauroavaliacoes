cc8bcce769b212777f1e6cc4a51604244e22e864
Alex Souza <59103542+allex-souza@users.noreply.github.com>
2023-07-07 17:30:24 -0300

    One Uploads
    a

---
+++ myjs.js
@@ -0,0 +1,3 @@
+function openWpp(){
+    window.open("https://api.whatsapp.com/send?phone=5562999719118&text=Ol%C3%A1!%20Mauro,%20gostaria%20de%20saber%20mais%20sobre%20os%20seus%20servi%C3%A7os.")
+}
